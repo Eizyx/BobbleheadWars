afe520ae5f0c2cc0562e981931ed91c116366da4
maclab030 <maclab030@maclab30.local>
2017-11-17 14:07:09 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 50.0f;
+	private CharacterController characterController;
+	public Rigidbody head;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		characterController.SimpleMove (moveDirection * moveSpeed);
+	}
+
+	void FixedUpdate(){
+		Vector3 moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		if (moveDirection == Vector3.zero) {
+			
+		} else {
+			head.AddForce (transform.right * 150, ForceMode.Acceleration);
+		}
+	}
+}
